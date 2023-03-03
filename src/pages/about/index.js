76dea34833727688078040b6b7f02c8d36c321cf
kikkawa <wakaki555@kikkawaatsutaka.com>
2023-03-03 15:12:33 +0900

    ✨ Aboutページを追加

---
+++ src/pages/about/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+import { Footer } from '@/components/Footer';
+import Links from '@/components/Links'
+
+export default function About() {
+  return (
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <h1 className={styles.title}>
+            AboutPage
+          </h1>
+
+          <p className={styles.description}>
+            Get started by editing <code>pages/about.js</code>
+          </p>
+
+          <Links />
+        </main>
+
+        <Footer />
+      </div>
+    </>
+  )
+}
