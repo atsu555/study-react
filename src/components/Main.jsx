f6d2d10d8713be6e73bccac49f2e3785fb0e2c3f
kikkawa <wakaki555@kikkawaatsutaka.com>
2023-03-04 23:28:51 +0900

    💄 styleファイルのリファクタリング

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,10 @@
 import { Links } from '@/components/Links';
 import { Headline } from '@/components/Headline';
-import styles from '@/styles/Home.module.css';
+import classes from './Main.module.css';
 
 export function Main(props) {
     return (
-        <main className={styles.main}>
+        <main className={classes.main}>
             <Headline page={props.page}>
                 <code>pages/{props.page}.js</code>
             </Headline>
