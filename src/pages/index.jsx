2dbf8c80cef79595e0486630fb33d674de737b09
atsutaka Akane <akane.kikkawaatsutaka@gmail.com>
2023-03-09 04:39:25 +0900

    クリックイベント書いてみた。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,14 @@ import styles from '@/styles/Home.module.css';
 import { Footer } from '@/components/Footer';
 import { Main } from '@/components/Main';
 import { Header } from '@/components/Header';
+import { useCallback } from 'react';
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e.target);
+    e.preventDefault();
+    alert("11")
+  }, [])
   return (
     <>
       <div className={styles.container}>
@@ -12,6 +18,7 @@ export default function Home() {
           <title>IndexPage</title>
         </Head>
         <Header />
+        <button onClick={handleClick}>ボタン</button>
 
         <Main page="index"/>
 
